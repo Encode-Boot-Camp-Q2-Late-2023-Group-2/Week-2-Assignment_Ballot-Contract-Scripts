57e471ae108b83d165350b7299586122343c1c2d
Andrew Prasaath <andrewprasaath07@gmail.com>
2023-05-08 00:15:49 +0530

    GiveRightToVote Script

--- scripts/2_GiveRightToVote.ts
+++ scripts/2_GiveRightToVote.ts
@@ -4,18 +4,16 @@ const contractAddress = "0x78b78280E0586b027a35F7feF0566DA734c39036";
 
 async function main() {
     const accounts = await ethers.getSigners();
-    const signer = accounts[0];
+    const deployer = accounts[0];
     const player = accounts[1];
 
     console.log(`Giving right to vote to "${player.address}"`);
-    const ballotContract = await ethers.getContractAt("Ballot", contractAddress, signer);
+    const ballotContract = await ethers.getContractAt("Ballot", contractAddress, deployer);
     const tx = await ballotContract.giveRightToVote(player.address);
     await tx.wait(1);
     const voter = await ballotContract.voters(player.address);
 
-    console.log(
-        `Voter with address "${player.address}" have right to vote with weight "${voter.weight}"`
-    );
+    console.log(`Voter "${player.address}" have right to vote with weight "${voter.weight}"`);
 }
 
 main().catch((error) => {
